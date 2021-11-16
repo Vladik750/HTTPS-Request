a81f0c0320360f8788ddc214bbc99ff225231e4f
Vlad Zhuk <zhuk.vlad2000@gmail.com>
2021-11-16 15:55:44 +0200

    Project location

---
+++ API/Core/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace API
+{
+    class Client
+    {
+        public RestClient client;
+
+        public Client()
+        {
+            client = new RestClient("https://swapi.dev/api/people");
+        }
+        
+    }
+}
